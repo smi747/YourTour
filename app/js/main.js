0d3ca548d86fe2bbe529216d12b15ca5c0bd1176
Michael Sladkov <sabpp@mail.ru>
2023-03-30 23:04:47 +0300

    HiddenMenu fully completed

--- app/js/main.js
+++ app/js/main.js
@@ -4,8 +4,10 @@ function bringmenu() {
     if (document.documentElement.scrollTop > 450) {
         document.getElementById('hiddenmenu').classList.add('show');
         document.querySelector('nav').classList.add('header__nav_nomarg');
+        document.getElementById('header__space').style.display= 'block';
     } else {
         document.getElementById('hiddenmenu').classList.remove('show');
         document.querySelector('nav').classList.remove('header__nav_nomarg');
+        document.getElementById('header__space').style.display = 'none';
     }
 }

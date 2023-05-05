6be2a978968dcb705fb928565e59143afb9e8a2a
Michael Sladkov <sabpp@mail.ru>
2023-05-05 19:50:26 +0300

    header fixed menu optimization

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,10 @@ window.addEventListener("scroll", bringmenu);
 
 function bringmenu() {
     if (document.documentElement.scrollTop > 450) {
-        document.getElementById('hiddenmenu').classList.add('show');
-        document.querySelector('nav').classList.add('header__nav_nomarg');
-        document.getElementById('header__space').style.display= 'block';
+        document.getElementById('header-menu').classList.add('show');
+        document.getElementById('header-space').style.display= 'block';
     } else {
-        document.getElementById('hiddenmenu').classList.remove('show');
-        document.querySelector('nav').classList.remove('header__nav_nomarg');
-        document.getElementById('header__space').style.display = 'none';
+        document.getElementById('header-menu').classList.remove('show');
+        document.getElementById('header-space').style.display = 'none';
     }
 }

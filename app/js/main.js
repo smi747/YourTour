3956339881143b411dedf82434c41f0b46fc9dfd
Michael Sladkov <sabpp@mail.ru>
2023-05-15 17:27:55 +0300

    code corrected with Prettier

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,11 @@
 window.addEventListener("scroll", bringmenu);
 
 function bringmenu() {
-    if (document.documentElement.scrollTop > 450) {
-        document.getElementById('header-menu').classList.add('show');
-        document.getElementById('header-space').style.display= 'block';
-    } else {
-        document.getElementById('header-menu').classList.remove('show');
-        document.getElementById('header-space').style.display = 'none';
-    }
+  if (document.documentElement.scrollTop > 450) {
+    document.getElementById("header-menu").classList.add("show");
+    document.getElementById("header-space").style.display = "block";
+  } else {
+    document.getElementById("header-menu").classList.remove("show");
+    document.getElementById("header-space").style.display = "none";
+  }
 }

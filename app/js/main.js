9126f95f50d8f373100f55bc7f60294db963ff56
Michael Sladkov <sabpp@mail.ru>
2023-05-19 18:06:38 +0300

    .header__top BEM fix

--- app/js/main.js
+++ app/js/main.js
@@ -2,10 +2,10 @@ window.addEventListener("scroll", bringmenu);
 
 function bringmenu() {
   if (document.documentElement.scrollTop > 450) {
-    document.getElementById("header-menu").classList.add("show");
+    document.getElementById("header-menu").classList.add("header__top_show");
     document.getElementById("header-space").style.display = "block";
   } else {
-    document.getElementById("header-menu").classList.remove("show");
+    document.getElementById("header-menu").classList.remove("header__top_show");
     document.getElementById("header-space").style.display = "none";
   }
 }
